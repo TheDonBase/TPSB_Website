c9d1faccc62eabb78e782a3f491b705560851475
TheDonBase <basebulldozer@gmail.com>
2025-06-13 19:27:57 +0200

    Fixed a typo

--- src/Controller/BotStatusController.php
+++ src/Controller/BotStatusController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 #[Route('/api')]
 class BotStatusController extends AbstractController
 {
-    #[Route('/bot/heartbeat', name: 'api_bot_heartbeat', methods: ['POST'])]
+    #[Route('/heartbeat', name: 'api_bot_heartbeat', methods: ['POST'])]
     public function heartbeat(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $botStatus = $entityManager->getRepository(BotStatus::class)->findOneBy([]) ?? new BotStatus();
